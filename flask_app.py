479e6ddef6bded7de45a68a1abfa04b113142bfe
Tomek <flotomek@gmail.com>
2021-05-05 09:37:10 +0000

    Update

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
 	if request.method == 'POST':
-		repo = git.Repo('path/to/git_repo')
+		repo = git.Repo('/home/tomflo/actions_test')
 		origin = repo.remotes.origin
 		origin.pull()
 
