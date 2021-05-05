3787825bd21c0a5ada0eab24f87d5bd0ed658317
Tomek <flotomek@gmail.com>
2021-05-05 09:20:34 +0000

    Connect Flask App to Repo

---
+++ flask_app.py
@@ -0,0 +1,22 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request
+import git
+
+app = Flask(__name__)
+@app.route('/update_server', methods=['POST'])
+def webhook():
+	if request.method == 'POST':
+		repo = git.Repo('path/to/git_repo')
+		origin = repo.remotes.origin
+		origin.pull()
+
+		return 'Updated PythonAnywhere successfully', 200
+	else:
+		return 'Wrong event type', 400
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
